61ca0423d3eda905e7b181eb33fd869ac211c8e3
Johannes Møgster <johannes.mogster@mojotech.no>
2020-09-21 14:50:13 +0200

    add work

---
+++ iiwa14_loadMesh.m
@@ -0,0 +1,34 @@
+function iiwa14_loadMesh()
+    %exampleHelperLoadSawyerMesh load sawyer robot mesh for visualization in Simscape Multibody
+    %
+    %   Set robot's mesh filepaths on the Simscape Multibody visualization blocks.
+
+    %   Copyright 2017-2019 The MathWorks, Inc.
+    
+    % Define path to Sawyer meshes
+    lbrMeshPath = fullfile('./','iiwa_description','meshes','iiwa14','visual');
+    
+    lbrL0Handle = 'iiwa14/KUKA LBR iiwa R820 14kg (iiwa14.urdf)/iiwa_link_0/Visual';
+    set_param(lbrL0Handle, 'ExtGeomFileName', fullfile(lbrMeshPath, 'link_0.stl')) ;
+    
+    lbrL1Handle = 'iiwa14/KUKA LBR iiwa R820 14kg (iiwa14.urdf)/iiwa_link_1/Visual';
+    set_param(lbrL1Handle, 'ExtGeomFileName', fullfile(lbrMeshPath, 'link_1.stl')) ;
+    
+    lbrL2Handle = 'iiwa14/KUKA LBR iiwa R820 14kg (iiwa14.urdf)/iiwa_link_2/Visual';
+    set_param(lbrL2Handle, 'ExtGeomFileName', fullfile(lbrMeshPath, 'link_2.stl')) ;
+    
+    lbrL3Handle = 'iiwa14/KUKA LBR iiwa R820 14kg (iiwa14.urdf)/iiwa_link_3/Visual';
+    set_param(lbrL3Handle, 'ExtGeomFileName', fullfile(lbrMeshPath, 'link_3.stl')) ;
+    
+    lbrL4Handle = 'iiwa14/KUKA LBR iiwa R820 14kg (iiwa14.urdf)/iiwa_link_4/Visual';
+    set_param(lbrL4Handle, 'ExtGeomFileName', fullfile(lbrMeshPath, 'link_4.stl')) ;
+    
+    lbrL5Handle = 'iiwa14/KUKA LBR iiwa R820 14kg (iiwa14.urdf)/iiwa_link_5/Visual';
+    set_param(lbrL5Handle, 'ExtGeomFileName', fullfile(lbrMeshPath, 'link_5.stl')) ;
+    
+    lbrL6Handle = 'iiwa14/KUKA LBR iiwa R820 14kg (iiwa14.urdf)/iiwa_link_6/Visual';
+    set_param(lbrL6Handle, 'ExtGeomFileName', fullfile(lbrMeshPath, 'link_6.stl')) ;
+    
+    lbrL7Handle = 'iiwa14/KUKA LBR iiwa R820 14kg (iiwa14.urdf)/iiwa_link_7/Visual';
+    set_param(lbrL7Handle, 'ExtGeomFileName', fullfile(lbrMeshPath, 'link_7.stl')) ;
+end
---
+++ sim_paraschos.m
@@ -0,0 +1,53 @@
+clear all; clc;
+
+%%
+addpath(genpath('./functions'))
+addpath(genpath('C:\Users\mojo\OneDrive\GitHub\ProMPLib'))
+
+%%
+load pp2ProMP.mat
+promp = pp2ProMP;
+
+dt = 0.001;
+
+tmp1 = load('controlGains_X-tremeFinal.mat');
+
+t_end = length(tmp1.q_mean)*dt;
+q_0 = tmp1.q_mean(1,:);
+
+%%
+lbr = importrobot('./iiwa_description/urdf/iiwa14.urdf');
+lbr.DataFormat = 'column';
+lbr.Gravity = [0, 0, -9.80665];
+
+%% load ProMP
+
+nr_steps = size(tmp1.K,3);
+tmp2 = 0:dt:nr_steps*dt-dt;
+
+% feedback_gain_K_t = timeseries(K,nr_steps);
+feedback_gain_K_t.time=tmp2;
+feedback_gain_K_t.signals.values=tmp1.K;
+feedback_gain_K_t.signals.dimensions=size(tmp1.K,[1,2]);
+
+% feed_forward_gain_k_t = timeseries(k,nr_steps);
+feed_forward_gain_k_t.time=tmp2;
+feed_forward_gain_k_t.signals.values=tmp1.k';
+feed_forward_gain_k_t.signals.dimensions=7;
+
+% epsilon_u = timeseries(k,nr_steps);
+epsilon_u.time = tmp2;
+epsilon_u.signals.values = tmp1.epsilon_u';
+epsilon_u.signals.dimensions = 7;
+
+clear tmp1 tmp2
+%%
+
+disp('sim..')
+string(datetime('now'))
+t_start = tic;
+simOut = sim('iiwa_paraschos','SimulationMode','normal','StopTime',string(t_end));
+toc(t_start)
+string(datetime('now'))
+
+disp('done')
