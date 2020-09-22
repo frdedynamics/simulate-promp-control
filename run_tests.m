b577996b99e749901cb5b678b9fa24a241959a8d
Johannes Møgster <johannes.mogster@mojotech.no>
2020-09-22 11:13:19 +0200

    refactor tests

--- run_tests.m
+++ run_tests.m
@@ -1,3 +1,4 @@
 clear all; clc; close all;
 
-test_result = runtests('tests/test_main.m')
+test_result = runtests('tests/test_main.m');
+disp(test_result.table)
--- tests/test_main.m
+++ tests/test_main.m
@@ -1,7 +1,24 @@
 
+% Known return values from exist(NAME)
+RETVAL_EXIST = [];
+RETVAL_EXIST.DOES_NOT_EXIST = 0;
+RETVAL_EXIST.IS_A_VARIABLE_IN_THE_WORKSPACE = 1;
+RETVAL_EXIST.IS_A_FILE = 2;
+RETVAL_EXIST.IS_A_MEX_FILE = 3;
+RETVAL_EXIST.IS_A_SIMULINK_MODEL_OR_LIB = 4;
+RETVAL_EXIST.IS_A_BUILT_IN_MATLAB_FUNCTION = 5;
+RETVAL_EXIST.IS_A_P_CODE_FILE = 6;
+RETVAL_EXIST.IS_A_FOLDER = 7;
+RETVAL_EXIST.IS_A_CLASS = 8;
+
 %% Check for ProMP existense
-assert(exist('ProMP.m','file') == 2, ...
-'ProMP.m file does not exist. did you open the project?')
+ERRID = 'MOJO:AssertionFailed:notExist';
+ERRMSG = 'ProMP.m file does not exist. Did you open the project?';
+EXPRESSION = exist('ProMP.m','file') == RETVAL_EXIST.IS_A_FILE;
+assert(EXPRESSION, ERRID, ERRMSG)
 
 %% Test other
-assert(2 == 2, '2 is not 2')
+ERRID = 'MOJO:AssertionFailed:notExist';
+ERRMSG = '2 is not 2';
+EXPRESSION = 2 == 2;
+assert(EXPRESSION, ERRID, ERRMSG)
